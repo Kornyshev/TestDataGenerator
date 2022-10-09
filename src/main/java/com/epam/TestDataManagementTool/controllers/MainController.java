174e67698d9399d7c03341021329f0c7b430e9f2
Evgeniy Kornyshev <Evgeniy_Kornyshev@epam.com>
2022-10-09 22:29:59 +0300

    UI elements work well

--- src/main/java/com/epam/TestDataManagementTool/controllers/MainController.java
+++ src/main/java/com/epam/TestDataManagementTool/controllers/MainController.java
@@ -1,10 +1,14 @@
 package com.epam.TestDataManagementTool.controllers;
 
 import com.epam.TestDataManagementTool.db.CustomersRepository;
+import com.epam.TestDataManagementTool.models.UiRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -13,10 +17,23 @@ public class MainController {
     CustomersRepository customersRepository;
 
     @GetMapping("/")
-    String getAllData(Model model) {
-        model.addAttribute("title","");
-        model.addAttribute("customersList", customersRepository.findAll());
-        return "table";
+    public String getIndex(Model model) {
+        model.addAttribute("uiRequest", new UiRequest());
+        return "index";
+    }
+
+    @PostMapping("/generate")
+    public ModelAndView generateDatabase(@ModelAttribute("uiRequest") UiRequest uiRequest) {
+        ModelAndView modelAndView = new ModelAndView();
+        /*
+        ProductLine -> Product
+        Office -> Employee
+        Customer -> Payment
+        Order -> OrderDetails
+        */
+
+        modelAndView.setViewName("table");
+        return modelAndView;
     }
 
 }
---
+++ src/main/java/com/epam/TestDataManagementTool/models/UiRequest.java
@@ -0,0 +1,30 @@
+package com.epam.TestDataManagementTool.models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UiRequest {
+
+    @Builder.Default
+    private int customersAmount = 100;
+    @Builder.Default
+    private int employeesAmount = 100;
+    @Builder.Default
+    private int officesAmount = 10;
+    @Builder.Default
+    private int ordersAmount = 10000;
+    @Builder.Default
+    private int orderDetailsAmount = 10000;
+    @Builder.Default
+    private int paymentsAmount = 5000;
+    @Builder.Default
+    private int productsAmount = 200;
+    @Builder.Default
+    private int productLinesAmount = 40;
+
+}
